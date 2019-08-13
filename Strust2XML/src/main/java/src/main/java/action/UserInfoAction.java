ae8443b87fb78c33849e57e2d549345a317e4d06
Phạm Thị Phượng - CMC Global DU2 <ptphuong3@ldap.cmcglobal.com.vn>
2019-08-13 17:43:11 +0900

    add project

---
+++ Strust2XML/src/main/java/src/main/java/action/UserInfoAction.java
@@ -0,0 +1,13 @@
+package src.main.java.action;
+import com.opensymphony.xwork2.ActionSupport;
+
+public class UserInfoAction extends ActionSupport{
+	private static final long serialVersionUID = 7299264265184515893L;
+    
+    
+    @Override
+    public String execute() {
+     
+        return "userInfoPage";
+    }
+}
